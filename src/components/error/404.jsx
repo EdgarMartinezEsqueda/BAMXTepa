d72d307a4c4a9eee098797ac954435d3c6bb10d7
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2024-05-21 14:01:20 -0600

    feat: updated 404 page

--- src/components/error/404.jsx
+++ src/components/error/404.jsx
@@ -1,9 +1,10 @@
-import img404 from "assets/404.jpg";
+import img404 from "assets/404.svg";
+
 const Error404 = () => {
     return (
       <section className="bg-bgClaro dark:bg-bgOscuro1 p-10">
         <div className="flex flex-col items-center justify-center ">
-          <img src={img404} alt="404" className="h-96" />
+          <img src={img404} className="h-96 fill-blue"/>
           <h1 className="text-4xl text-center">
             Página no encontrada
           </h1>
