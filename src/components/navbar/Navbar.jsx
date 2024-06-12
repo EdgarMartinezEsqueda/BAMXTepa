9ad67a38d6a1a34b58e8b6d5efdea024f1888558
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2024-06-12 15:16:25 -0600

    refactor: navbar updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => { 
   return (
-    <header aria-label="Site Header" className="border-b sticky top-0 py-2 ">
+    <header id="site-header" aria-label="Site Header" className="sticky top-0 py-2 z-10 bg-gray-100 ">
       <div className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8" >
         <div className="flex items-center gap-4">
           <a href="/" className="flex">
@@ -16,20 +16,20 @@ const Header = () => {
           </a>
         </div>
         <div className="flex flex-1 items-center justify-end gap-8">
-          <nav aria-label="Site Nav" id="navbar" className={`fixed top-0 left-0 right-0 bottom-0 flex text-center justify-center flex-col max-lg:bg-black max-lg:text-white max-lg:opacity-80 max-lg:w-screen max-lg:h-screen max-lg:top-0 max-lg:left-0 lg:static lg:flex-row lg:gap-4 text-xl max-md:text-lg lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500`} >
-            <a href="/nosotros" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-rojoLogo opacity-100" >
+          <nav aria-label="Site Nav" id="navbar" className={`fixed top-0 left-0 right-0 bottom-0 flex text-center justify-center flex-col text-grisLogo max-lg:opacity-80 max-lg:w-screen max-lg:h-screen max-lg:top-0 max-lg:left-0 lg:static lg:flex-row lg:gap-4 text-xl max-md:text-lg lg:font-bold lg:uppercase lg:tracking-wide`} >
+            <a href="/nosotros" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-amarilloLogo opacity-100" >
               Nosotros
             </a>
-            <a href="/donar" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-rojoLogo opacity-100" >
-              Donación
-            </a>
-            <a href="/programas" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-rojoLogo opacity-100" >
+            <a href="/programas" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-amarilloLogo opacity-100" >
               Programas
             </a>
-            <a href="/voluntario" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-rojoLogo opacity-100" >
+            <a href="/voluntario" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-amarilloLogo opacity-100" >
               Voluntariado
             </a>
-            <a href="/contacto" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-rojoLogo opacity-100" >
+            <a href="/donar" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-amarilloLogo opacity-100" >
+              Donación
+            </a>
+            <a href="/contacto" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-amarilloLogo opacity-100" >
               Contacto
             </a>
           </nav>
