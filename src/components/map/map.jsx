c7d8a958ca433563deacd05d38f9d88d784a23f8
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2024-12-31 13:01:50 -0600

    refactor: Map description updated

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -24,9 +24,12 @@ export default function Mapa(){
         <div className="md:flex md:items-center">
             <div className="p-8 lg:p-32 md:w-2/3">
                 <h2 className="font-bold text-verdeLogo text-4xl max-md:text-center max-md:w-full">Municipios a los que llegamos</h2>
-                <p className=" max-md:hidden text-xl mt-8">En nuestro compromiso por combatir el hambre, llegamos a más de 85 comunidades en 14 municipios de la región (13 en Jalisco y 1 en Zacatecas) en las que cada gesto de solidaridad marca la diferencia. </p>
-                <p className=" max-md:hidden text-xl my-1">Desde pequeños poblados hasta barrios urbanos, nuestro banco de alimentos trabaja incansablemente para llevar esperanza y nutrición a quienes más lo necesitan.</p>
-                <p className=" max-md:hidden text-xl my-1">Conoce más sobre las comunidades a las que llegamos y únete a nuestra causa.</p>
+                <p className="text-xl mt-8">
+                    En nuestro compromiso por combatir el hambre, llegamos a más de 85 comunidades en 14 municipios de la región (13 en Jalisco y 1 en Zacatecas).
+                </p>
+                <p className=" max-md:hidden text-xl my-1">
+                    Desde pequeños poblados hasta barrios urbanos, trabajamos incansablemente para llevar esperanza y nutrición.
+                </p>
             </div>
             <div className="w-full max-md:w-11/12 max-md:mx-auto lg:w-1/3">
             <HighchartsReact 
