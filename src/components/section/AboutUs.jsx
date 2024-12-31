5f861c4c5ae0f00f1a441e7532f82f9de181f032
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2024-12-31 14:08:17 -0600

    refactor: About Us updated

--- src/components/section/AboutUs.jsx
+++ src/components/section/AboutUs.jsx
@@ -7,10 +7,10 @@ const AboutUs = () => {
             <h2 className="text-2xl font-black text-white sm:text-3xl md:text-5xl text-center">
               Acerca de nosotros
             </h2>
-            <p className="text-white md:block md:text-xl md:leading-relaxed text-center" >
+            <p className="text-white md:block md:text-xl md:leading-relaxed text-center w-3/4" >
               Somos una asociación civil dedicada a rescatar alimentos para fortalecer la alimentación de comunidades en la región de los Altos de Jalisco.
             </p>
-            <p className="text-white md:block md:text-xl md:leading-relaxed text-center max-sm:hidden" >
+            <p className="text-white md:block md:text-xl md:leading-relaxed text-center w-3/4 max-sm:hidden" >
               Nuestro compromiso es reducir el desperdicio de alimentos y combatir el hambre, trabajando en conjunto con empresas, instituciones y la comunidad para crear un impacto positivo y sostenible.
             </p>
           </div>
