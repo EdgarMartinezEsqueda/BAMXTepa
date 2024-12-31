ad47537ac75e31482135e7207f3752a319b68bae
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2024-12-31 14:26:57 -0600

    refactor: AboutUs page updated

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -8,7 +8,13 @@ import OurTeam from "../components/section/OurTeam";
 // Componente para la sección de Historia
 const Historia = () => (
     <section className="text-xl ">
-      <h1 className="text-5xl text-grisLogo  text-center p-8">Nuestra Historia</h1>
+      <h1 className="text-5xl text-verdeLogo text-center pt-8">Nuestra Historia</h1>
+      <p className="text-center md:text-xl lg:text-2xl">
+        Desde 2007, hemos trabajado incansablemente para combatir el hambre en nuestra región.
+      </p>
+      <p className="text-center md:text-xl lg:text-2xl pb-4 max-sm:hidden">
+        Descubre cómo hemos crecido y evolucionado a través de los años.
+      </p>
       <Carousel />
     </section>
   );
