d018ddb5bddd877e9897b7e368258d7bd3d70ba5
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-01-02 14:19:59 -0600

    refactor: updated 404 page

--- src/components/error/404.jsx
+++ src/components/error/404.jsx
@@ -5,7 +5,7 @@ const Error404 = () => {
       <section className="bg-white dark:bg-bgOscuro1 p-10">
         <div className="flex flex-col items-center justify-center ">
           <img src={img404} className="h-96 fill-blue"/>
-          <h1 className="text-4xl text-center">
+          <h1 className="text-4xl text-center font-omnes font-bold p-12">
             Página no encontrada
           </h1>
         </div>
