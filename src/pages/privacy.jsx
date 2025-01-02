111fb3022784b99a9091fdd21672e625f7af0cf7
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-01-02 10:04:23 -0600

    feat: Added Privacy Policy

---
+++ src/pages/privacy.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
+import Privacy from "../components/privacy/Privacy";
+import Footer from "../components/footer/Footer";
+
+const FAQPage = () =>{
+    return (
+      <>
+        <Navbar/>
+        <Privacy/>
+        <Footer/>
+      </>
+    );
+};
+
+export default FAQPage;
